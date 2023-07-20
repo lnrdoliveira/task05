ca0a9a558b966de51da3f77eb3c657505029e0ce
Leonardo <lnrdoliveira.dev@gmail.com>
2023-07-20 15:04:08 -0300

    Exercício Funcionário

---
+++ Funcionario.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System. Globalization;
+
+namespace task05
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido(){
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem){
+            SalarioBruto  = SalarioBruto + (SalarioBruto * porcentagem)/100.0;
+        }
+        public override string ToString()
+        {
+            return Nome+", $"+SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using task05;
+
+namespace Task05{
+    class Program{
+        static void Main(String[] args){
+            Funcionario f = new Funcionario();
+
+            System.Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            System.Console.Write("Salário Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+            System.Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("Funcionário: "+f);
+            System.Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double Porcentagem = double.Parse(Console.ReadLine());
+            f.AumentarSalario(Porcentagem);
+            System.Console.WriteLine("Dados atualizado: "+f);
+        }
+    }
+}
